f56f4aa6b49faa20d8fcecc20225ff74aa064449
floppydisk89 <53581002+floppydisk89@users.noreply.github.com>
2022-09-07 16:20:24 +0200

    added prisma

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -1,13 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // const { name, email, posts } = request.body;
+
+  // const postData = posts?.map((post: Prisma.PostCreateInput) => {
+  //   return { title: post?.title, content: post?.content };
+  // });
+
+  // const result = await prisma.user.create({
+  //   data: {
+  //     name,
+  //     email,
+  //     posts: {
+  //       create: postData,
+  //     },
+  //   },
+  // });
+  res.status(200).json({ name: 'John Doe' });
 }
