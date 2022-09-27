41310b6b01dee5ab2a6b86392a9a0515d8ce8d86
floppydisk89 <53581002+floppydisk89@users.noreply.github.com>
2022-09-27 15:26:26 +0200

    prisma example update

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -3,28 +3,48 @@ import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+//* typing example, it should be more complete than this.
+
 type Data = {
-  name: string;
+  message?: string;
+  error?: string;
+  data?: object | null;
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse<Data>,
 ) {
   // const { name, email, posts } = request.body;
 
-  // const postData = posts?.map((post: Prisma.PostCreateInput) => {
-  //   return { title: post?.title, content: post?.content };
-  // });
+  //* Prisma data registry and retrieval example.
 
-  // const result = await prisma.user.create({
-  //   data: {
-  //     name,
-  //     email,
-  //     posts: {
-  //       create: postData,
-  //     },
-  //   },
-  // });
-  res.status(200).json({ name: 'John Doe' });
+  try {
+    await prisma.user
+      .create({
+        data: {
+          name: 'Floppydisk89',
+          email: 'thisisfloppyemail@floppy89.com',
+        },
+      })
+      .then(() => {
+        console.log({ message: 'registered user - OK' });
+      });
+  } catch (err) {
+    console.error('user already exists');
+  }
+
+  try {
+    await prisma.user
+      .findUnique({
+        where: {
+          email: 'thisisfloppyemail@floppy89.com',
+        },
+      })
+      .then(data => {
+        response.status(200).json({ message: 'Found a user', data: data });
+      });
+  } catch (err) {
+    response.status(500).json({ message: 'unable to retrieve a user.' });
+  }
 }
