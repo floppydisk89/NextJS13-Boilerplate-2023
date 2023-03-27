658f5c5e1d863cc9e67eb5f1132770b575ee2af8
floppydisk89 <53581002+floppydisk89@users.noreply.github.com>
2023-03-27 15:23:00 +0200

    wip

--- cypress/e2e/server.cy.js
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-import host from '../../data/host';
-
-describe('Live Server', () => {
-  it('should visit', () => {
-    cy.visit(`${host.baseUrl.development}/`);
-  });
-});
